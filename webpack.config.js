a3e9c204c1cebd8ca93dc5a81abbfd8785ba7172
zeroaz2020 <godmarketing1004@gmail.com>
2020-05-20 17:35:20 +0900

    Setting Completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+require('@babel/polyfill')
 
 module.exports = {
     // 진입점
     entry: {
-        app: path.join(__dirname, 'main.js')
+        app: [
+            '@babel/polyfill',
+            path.join(__dirname, 'main.js')
+        ]
     },
     // 결과물에 대한 설정
     output: {
@@ -21,15 +26,21 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: 'babel-loader',
             },
             {
                 test: /\.css$/,
-                use: ["vue-style-loader", "css-loader"]
+                use: [
+                    "vue-style-loader",
+                    "css-loader"
+                ]
             }
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+                template: path.join(__dirname, 'index.html')
+            })
     ]
 }
