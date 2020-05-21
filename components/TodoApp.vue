3708f64e9e6f2e12118c443b81de88662a448e79
zeroaz2020 <godmarketing1004@gmail.com>
2020-05-21 18:11:57 +0900

    Local DB 초기화-Lowdb와 Lodash 설치 및 DB초기화

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-        <todo-item />
-        <todo-creator />
+        <todo-item/>
+        <todo-creator/>
     </div>
 </template>
 
 <script>
+    import lowdb from 'lowdb'
+    import LocalStorage from 'lowdb/adapters/LocalStorage'
     import TodoCreator from './TodoCreator'
     import TodoItem from './TodoItem'
 
@@ -13,6 +15,25 @@
         components: {
             TodoCreator,
             TodoItem
+        },
+        data() {
+            db: null
+        },
+        created () {
+            this.initDB()
+        },
+        methods: {
+            initDB() {
+                const adapter = new LocalStorage('todo-app')  // DB
+                this.db = lowdb(adapter)
+
+                // Local DB 초기화
+                this.db
+                    .defaults({
+                        todos: [] // Collection
+                    })
+                    .write()
+            }
         }
     }
 </script>
